bbd635f5d80db5fb5046a368dfdf01bc023ba2f0
Thomas Wilder <tmwilder@outlook.com>
2023-11-03 16:23:20 +0000

    feat: can now mean more than one variable and the user can choose whether they want the sqd variable or not.

--- ORCA025/pyCDFTOOLS/cdfmoy.py
+++ ORCA025/pyCDFTOOLS/cdfmoy.py
@@ -28,16 +28,16 @@ import xarray as xr
 import os
 import re
 
-def cdfmoy(data_directory, filename, var_name, new_filename, **kwargs):
+def cdfmoy(data_directory, filename, variable_names, new_filename, **kwargs):
     """
     Computes:
-    1) mean of a supplied variable
-    2) mean of a variable squared
+    1) mean of supplied variables
+    2) mean of variables squared
 
     Inputs:
       data_dir   = string for data directory
       filename   = string of a single or list of filenames on the same grid
-      var_name   = string for variable name
+      var_name   = string/list for variable name
       new_filename = a filename that could be of form cdfmoy*
 
     Optional arguments:
@@ -52,10 +52,12 @@ def cdfmoy(data_directory, filename, var_name, new_filename, **kwargs):
 
     History:
       Sep 2023     First write up in Python.
+      Nov          Added user defined option for square of mean.
+                   Multiple variable names enabled
     """
     
     # some defaults for optional keyword arguments
-    opt_dic = {"lprint": False, "chunk": 6, "surf": False, "grid": "U"}
+    opt_dic = {"lprint": False, "chunk": 6, "surf": False, "grid": "U", "sqd": False}
 
     # overwrite the options by cycling through the input dictionary
     for key in kwargs:
@@ -81,23 +83,39 @@ def cdfmoy(data_directory, filename, var_name, new_filename, **kwargs):
         
     elif len(filename) < 2: # filename is one file with multiple time indices
         
-        ds = xr.open_dataset(data_directory + filename, chunks={"time_counter": "chunk"})
+        ds = xr.open_dataset(data_directory + filename, chunks={"time_counter": 1})
         if opt_dic["lprint"]:
             print(ds)
     
 
-    print(var_name)
-    # mean velocity data
-    var = ds[var_name].mean(dim="time_counter")
-    print('mean computed')
+    print(variable_names)
+    
+    # mean data
+    # empty dictionary to store results
+    means = {} 
+
+    # loop and compute means
+    for var_name in variable_names:
+        means[var_name] = ds[var_name].mean(dim="time_counter")
+    print('means computed')
     # print(var)
 
+    # create new dataset from means
+    means_ds = xr.Dataset(means)
+    
     # square velocity data and mean
-    var_sqd = (ds[var_name] ** 2).mean(dim="time_counter")
-    var_sqd = var_sqd.rename(var_name + "2")
+    if opt_dic["sqd"]:
+        means_sqd = {}
+
+        for var_names in variable_names:
+            means_sqd[var_name + "2"] = (ds[var_name] ** 2).mean(dim="time_counter")
+            # var_sqd = var_sqd.rename(var_name + "2")
+
+        means_sqd_ds = xr.Dataset(means_sqd)
     
     # write data to netcdf
-    var.to_netcdf(data_directory + new_filename + ".nc")
-    var_sqd.to_netcdf(data_directory + new_filename + "2.nc")
+    means_ds.to_netcdf(data_directory + new_filename + ".nc")
+    if opt_dic["sqd"]:
+        means_sqd_ds.to_netcdf(data_directory + new_filename + "2.nc")
     
     return print('variables saved to netcdf')
